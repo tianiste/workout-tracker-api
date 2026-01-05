c7b2000ade3563943fae714ff1e38944511a7bd4
Tian Istenič <tian.istenic34@gmail.com>
2026-01-05 20:31:17 +0100

    feat: added ping route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "workout-tracker/db"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	db.InitDB()
+	fmt.Println("hello")
+	router := gin.Default()
+
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	router.Run()
 }

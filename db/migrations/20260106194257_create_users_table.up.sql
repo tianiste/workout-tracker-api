b40fe11b42f1b890d87b8667d268e72a5582680d
Tian Istenič <tian.istenic34@gmail.com>
2026-01-08 19:03:53 +0100

    chore: fixed migrations

--- db/migrations/20260106194257_create_users_table.up.sql
+++ db/migrations/20260106194257_create_users_table.up.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXIST Users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT
-  name TEXT NOT NULL UNIQUE
-  pass_hash TEXT NOT NULL
-  created_at TEXT NOT NULL DEFAULT(strftime('%Y-%m-%dT%H:%M:%SZ', 'now'))
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL UNIQUE,
+  pass_hash TEXT NOT NULL,
+  created_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ', 'now'))
 );
+

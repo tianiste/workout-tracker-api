2e7e9090eaf2803593ba6ff8c1c8c9e3504b68f1
Tian Istenič <tian.istenic34@gmail.com>
2026-01-08 19:46:24 +0100

    feat: added password checking to userservice

--- internal/services/userService.go
+++ internal/services/userService.go
@@ -5,9 +5,12 @@ import (
 	"errors"
 	"strings"
 	"workout-tracker/internal/repo"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var ErrRequiredFields = errors.New("required fields are empty")
+var ErrInvalidCredentials = errors.New("invalid credentials")
 
 type UserService struct {
 	Repo *repo.UserRepo
@@ -24,6 +27,21 @@ func (service *UserService) validateCredentials(name, password string) error {
 	return nil
 }
 
+func (service *UserService) hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func (service *UserService) comparePasswords(password, hash string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(password), []byte(hash)); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (service *UserService) Login(name, password string) (string, error) { // returns jwt as string
 	if err := service.validateCredentials(name, password); err != nil {
 		return "", err
@@ -35,7 +53,9 @@ func (service *UserService) Login(name, password string) (string, error) { // re
 		}
 		return "", err
 	}
-	// check if passwords match
+	if err := service.comparePasswords(password, user.PasswordHash); err != nil {
+		return "", ErrInvalidCredentials
+	}
 	// return token
-
+	return user.Username, nil // placeholder
 }

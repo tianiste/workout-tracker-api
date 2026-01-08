1f842589f8da449d101f4f4175c9ed72ffc03cfc
Tian Istenič <tian.istenic34@gmail.com>
2026-01-08 20:15:58 +0100

    chore: improoved jwts

--- internal/services/userService.go
+++ internal/services/userService.go
@@ -3,9 +3,15 @@ package services
 import (
 	"database/sql"
 	"errors"
+	"fmt"
+	"os"
+	"strconv"
 	"strings"
+	"time"
+	"workout-tracker/internal/models"
 	"workout-tracker/internal/repo"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -21,7 +27,7 @@ func NewUserService(repo *repo.UserRepo) *UserService {
 }
 
 func (service *UserService) validateCredentials(name, password string) error {
-	if strings.TrimSpace(name) == "" && strings.TrimSpace(password) == "" {
+	if strings.TrimSpace(name) == "" || strings.TrimSpace(password) == "" {
 		return ErrRequiredFields
 	}
 	return nil
@@ -42,6 +48,43 @@ func (service *UserService) comparePasswords(password, hash string) error {
 	return nil
 }
 
+var SecretKey = []byte(os.Getenv("JWT_KEY"))
+
+func (service *UserService) createToken(user *models.User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username": user.Username,
+			"iat":      time.Now().Unix(),
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+			"sub":      strconv.FormatInt(user.Id, 10),
+		})
+	tokenString, err := token.SignedString(SecretKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func (service *UserService) verifyToken(tokenString string) (jwt.MapClaims, error) {
+	claims := jwt.MapClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		if len(SecretKey) == 0 {
+			return nil, fmt.Errorf("jwt secret key is not set")
+		}
+		return SecretKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+	return claims, nil
+}
+
 func (service *UserService) Login(name, password string) (string, error) { // returns jwt as string
 	if err := service.validateCredentials(name, password); err != nil {
 		return "", err
@@ -49,13 +92,17 @@ func (service *UserService) Login(name, password string) (string, error) { // re
 	user, err := service.Repo.GetUserByName(name)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return "", ErrRequiredFields
+			return "", ErrInvalidCredentials
 		}
 		return "", err
 	}
 	if err := service.comparePasswords(password, user.PasswordHash); err != nil {
 		return "", ErrInvalidCredentials
 	}
-	// return token
-	return user.Username, nil // placeholder
+	token, err := service.createToken(&user)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil // placeholder
 }
